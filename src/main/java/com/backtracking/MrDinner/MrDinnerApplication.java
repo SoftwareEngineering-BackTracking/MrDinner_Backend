67546aa69d8b640668d581ef24865a6731cff9df
naingyu <sktmdgus1212@naver.com>
2022-10-22 17:46:16 +0900

    solving git problem

--- src/main/java/com/backtracking/MrDinner/MrDinnerApplication.java
+++ src/main/java/com/backtracking/MrDinner/MrDinnerApplication.java
@@ -2,13 +2,10 @@ package com.backtracking.MrDinner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-<<<<<<< HEAD
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
-=======
 
->>>>>>> ad7ccf8986d93659b845472d827da917ed6b0206
 @SpringBootApplication
 public class MrDinnerApplication {
 
